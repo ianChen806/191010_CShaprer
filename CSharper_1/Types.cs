c39ce648ece581f8785ef38274851c8d6642bb75
Ian <rutsuki123@gmail.com>
2019-10-13 17:53:09 +0800

    add filed sample.

--- CSharper_1/Types.cs
+++ CSharper_1/Types.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace CSharper_1
 {
     public class PrimitiveType
@@ -5,12 +7,17 @@ namespace CSharper_1
         void Main()
         {
             int a1 = 1;
-            
+
             System.Int32 a2 = 1;
-            
+
             int a3 = new int();
-            
+
             System.Int32 a4 = new System.Int32();
+
+            Console.WriteLine(a1);
+            Console.WriteLine(a2);
+            Console.WriteLine(a3);
+            Console.WriteLine(a4);
         }
     }
 
--- FiledSample/Program.cs
+++ FiledSample/Program.cs
@@ -7,10 +7,35 @@ namespace FiledSample
         static void Main(string[] args)
         {
             var myClass = new MyClass();
+            // var myClass = new MyClass("name");
+            // var myClass = new MyClass("name",123);
+
+            var name = myClass.GetName();
+
+            Console.WriteLine(name);
         }
     }
 
     internal class MyClass
     {
+        private readonly string _name = "123";
+
+        public MyClass()
+        {
+            _name = "3455";
+        }
+
+        public MyClass(string name)
+        {
+        }
+
+        public MyClass(string name, int index)
+        {
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
     }
 }
