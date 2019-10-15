4a717cd35006b087b9e9b7277687808209f147e5
Ian <rutsuki123@gmail.com>
2019-10-15 21:44:19 +0800

    add delegate sample.

---
+++ DelegateSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DelegateSample
+{
+    public delegate string ShowNameFunction(string name);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            var name = myClass.ShowName(Add);
+
+            Console.WriteLine(name);
+        }
+
+        private static string Add(string value)
+        {
+            return $"my name: {value}";
+        }
+    }
+
+    internal class MyClass
+    {
+        private string _name = "Test";
+
+        public string ShowName(ShowNameFunction function)
+        {
+            return function.Invoke(_name);
+        }
+    }
+}
---
+++ DelegateSample_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegateSample_2
+{
+    public delegate string ShowNameFunction(string name);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            var name = myClass.ShowName(value => $"my name: {value}");
+
+            Console.WriteLine(name);
+        }
+    }
+
+    internal class MyClass
+    {
+        private string _name = "Test";
+
+        public string ShowName(ShowNameFunction function)
+        {
+            return function.Invoke(_name);
+        }
+    }
+}
