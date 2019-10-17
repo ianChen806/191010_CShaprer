ba11cdf763f1df2a90fb02abd4947a042df8d57d
Ian <rutsuki123@gmail.com>
2019-10-18 07:34:38 +0800

    add CtorSample2.

---
+++ ConstructorSample_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConstructorSample_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new RecordService(1, 50);
+        }
+    }
+
+    internal class RecordService
+    {
+        private readonly int _page;
+        private readonly int _pageSize;
+
+        public RecordService(int page, int pageSize)
+        {
+            _page = page;
+            _pageSize = pageSize;
+        }
+    }
+}
