bde6646b2f5e6c1774d70bf4d53b8bd2bcc2efd1
Ian <rutsuki123@gmail.com>
2019-10-18 07:22:40 +0800

    add filedSample2.

---
+++ FiledSample_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FiledSample_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new KeyGenerate(123);
+
+            var id = myClass.GetKey();
+
+            Console.WriteLine(id);
+        }
+    }
+
+    internal class KeyGenerate
+    {
+        private readonly int _id;
+
+        public KeyGenerate(int id)
+        {
+            _id = id;
+        }
+
+        public string GetKey()
+        {
+            return $"MyClass-{_id}-{DateTime.Now.Ticks}";
+        }
+    }
+}
