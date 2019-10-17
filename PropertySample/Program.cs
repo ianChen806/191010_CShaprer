90e6f604a89b730d5adaac94d5f5613b42d5b819
Ian <rutsuki123@gmail.com>
2019-10-18 07:48:34 +0800

    add PropertySample.

--- PropertySample/Program.cs
+++ PropertySample/Program.cs
@@ -15,7 +15,18 @@ namespace PropertySample
 
     internal class MyClass
     {
-        public string Name { get; set; }
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
     }
-    
 }
