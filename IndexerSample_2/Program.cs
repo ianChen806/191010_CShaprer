12389c8da0f052f80d8a24a5c3ecd08b280f4e48
Ian <rutsuki123@gmail.com>
2019-10-19 22:59:37 +0800

    add IndexerSample_2.

---
+++ IndexerSample_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace IndexerSample_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            myClass["Name"] = "Test";
+            myClass["IP"] = "127.0.0.1";
+        }
+    }
+
+    internal class MyClass
+    {
+        private Dictionary<string, string> _values = new Dictionary<string, string>();
+        
+        public string this[string ip]
+        {
+            get { return _values[ip]; }
+            set
+            {
+                if (_values.ContainsKey(ip) == false)
+                {
+                    _values.Add(ip, value);
+                }
+                else
+                {
+                    _values[ip] = value;
+                }
+            }
+        }
+    }
+}
