b335741a314f47d81fd786adb78aca526b73960d
Ian <rutsuki123@gmail.com>
2019-10-19 22:52:11 +0800

    add IndexerSample.

---
+++ IndexerSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IndexerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringCollection = new SampleCollection();
+            stringCollection[0] = "Hello, World";
+            
+            Console.WriteLine(stringCollection[0]);
+        }
+    }
+
+    internal class SampleCollection
+    {
+        private string[] values = new string[100];
+
+        public string this[int i]
+        {
+            get
+            {
+                return values[i];
+            }
+            set
+            {
+                values[i] = value;
+            }
+        }
+    }
+}
