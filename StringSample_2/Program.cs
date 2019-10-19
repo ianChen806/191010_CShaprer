d1587cac2977419aa9a3cdd95344444999c7ba3c
Ian <rutsuki123@gmail.com>
2019-10-19 22:48:20 +0800

    add StringSample_2.

---
+++ StringSample_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace StringSample_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("Test");
+            stringBuilder.Append("123");
+            stringBuilder.Append("456");
+            var value = stringBuilder.ToString();
+
+            Console.WriteLine(value);
+        }
+    }
+}
