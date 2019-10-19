f3422967c3cbe2f04178126a92ee9183847c85e8
Ian <rutsuki123@gmail.com>
2019-10-19 23:11:19 +0800

    add NestedTypeSample1~2.

---
+++ NestedTypeSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NestedTypeSample_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+
+            var myClass2 = new MyClass.MyClass2();
+        }
+    }
+
+    internal class MyClass
+    {
+        enum MyEnum
+        {
+        }
+
+        public class MyClass2
+        {
+        }
+
+        interface IInterface
+        {
+        }
+    }
+}
---
+++ NestedTypeSample_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NestedTypeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+
+            var key = myClass.GetKey();
+
+            Console.WriteLine(key);
+        }
+    }
+
+    internal class MyClass
+    {
+        private readonly KeyGenerator _keyGenerator = new KeyGenerator();
+
+        public string GetKey()
+        {
+            return _keyGenerator.Get();
+        }
+
+        private class KeyGenerator
+        {
+            private readonly string _key = "private key";
+
+            public string Get()
+            {
+                return $"{_key}-{DateTime.Now.Ticks}";
+            }
+        }
+    }
+}
