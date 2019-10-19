0fe1299811422da1c0a5676e6c8c73e9741f748e
Ian <rutsuki123@gmail.com>
2019-10-20 00:07:34 +0800

    add StaticSample.

---
+++ StaticSample/Md5Helper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace StaticSample
+{
+    internal class Md5Helper
+    {
+        public static string Hash(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StaticSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StaticSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = MyClass.GetKey();
+            Console.WriteLine(key);
+
+            var value = "123";
+            var result = value.Md5Hash();
+        }
+    }
+
+    internal static class MyClass
+    {
+        private static string _key = "123";
+
+        static MyClass()
+        {
+            Console.WriteLine("ctor");
+        }
+
+        public static string GetKey()
+        {
+            return _key;
+        }
+        
+        public static string Md5Hash(this string value)
+        {
+            return Md5Helper.Hash(value);
+        }
+    }
+}
