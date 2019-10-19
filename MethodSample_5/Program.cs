d48a1496c22fcaabbd67aebe6b4436da438cb90a
Ian <rutsuki123@gmail.com>
2019-10-19 23:45:36 +0800

    add MethodSample_5

---
+++ MethodSample_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace MethodSample_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            var result = myClass.Add("Total", 1, 2, 3, 4, 5);
+
+            Console.WriteLine(result);
+        }
+    }
+
+    internal class MyClass
+    {
+        public string Add(string key, params int[] values)
+        {
+            return $"{key}={values.Sum()}";
+        }
+
+        // public string Add(params int[] values, string key)
+        // {
+        //     return $"{key}={values.Sum()}";
+        // }
+    }
+}
