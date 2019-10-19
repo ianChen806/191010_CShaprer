1a81e39de70866c4e36a71f8b1c00301b8bfdd90
Ian <rutsuki123@gmail.com>
2019-10-19 22:45:45 +0800

    add StringSample.

---
+++ StringSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value1 = "123";
+            
+            var value2 = "ABC";
+            
+            var value3 = "123";
+        }
+    }
+}
