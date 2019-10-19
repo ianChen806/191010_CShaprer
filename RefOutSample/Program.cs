75a85c27936b7ece5d5deab8751977e228fd7fec
Ian <rutsuki123@gmail.com>
2019-10-19 22:40:14 +0800

    add RefOutSample.

---
+++ RefOutSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RefOutSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+
+            int value1 = 1;
+            myClass.Ref(ref value1);
+            Console.WriteLine($"value1 = {value1}");
+
+            int value2;
+            myClass.Out(out value2);
+            Console.WriteLine($"value2 = {value2}");
+        }
+    }
+
+    internal class MyClass
+    {
+        public void Ref(ref int value)
+        {
+            value = 5;
+        }
+
+        public void Out(out int value2)
+        {
+            value2 = 10;
+        }
+    }
+}
