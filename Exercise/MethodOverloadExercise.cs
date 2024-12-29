04e8505449b43519af119d06e35644286cba08ae
IanChen <ian806.chen@gmail.com>
2024-12-29 20:56:40 +0800

    add exercise

---
+++ Exercise/MethodOverloadExercise.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace Exercise;
+
+public class MethodOverloadExercise
+{
+    [Fact]
+    public void Method()
+    {
+        // 實現一組方法, 可以計算 int、decimal、double 三種型別的加法
+        var calculator = new Calculator();
+
+        // calculator.Add(1, 2).Should().Be(3);
+        // calculator.Add(1.1, 2.2).Should().Be(3.3);
+        // calculator.Add(1.1m, 2.2m).Should().Be(3.3m);
+    }
+
+    private class Calculator
+    {
+        // TODO
+    }
+}
