7e4b93d5f40bdaf14c98473ff28eb2cb6c0a430a
IanChen <ian806.chen@gmail.com>
2024-12-29 20:20:28 +0800

    調整註解

--- Exercise/AccessModifierExercise.cs
+++ Exercise/AccessModifierExercise.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercise;
+﻿using FluentAssertions;
+
+namespace Exercise;
 
 public class AccessModifierExercise
 {
@@ -7,23 +9,29 @@ public class AccessModifierExercise
     {
         var member = new Member(id: 1, name: "test");
 
-        // TODO
-        // don't get
+        // 保持不能直接取得 Id
         // var id = member.Id;
 
-        // pass this
+        // 只能夠過方法取得 Id
         // var id = member.GetId();
         // id.Should().Be(1);
 
-        // pass this
+        // 可以直接取得 Name
         // member.Name.Should().Be("test");
     }
 
     private class Member
     {
+        //TODO
+        int Id { get; set; }
+
+        //TODO
+        string Name { get; set; }
+
         public Member(int id, string name)
         {
-            // TODO
+            Id = id;
+            Name = name;
         }
     }
 }
