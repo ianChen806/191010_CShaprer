8b857ffdc17ae1c576bc6dc3097b28fb2d422f4b
IanChen <ian806.chen@gmail.com>
2024-12-30 10:57:57 +0800

    add exercise

---
+++ Exercise/RefOutExercise.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercise;
+
+public class RefOutExercise
+{
+    [Fact]
+    public void out_Pass()
+    {
+        var str = "10";
+        int result;
+        var target = new CustomInt();
+
+        // TODO 實現一個 int.TryParse
+        // var parse = target.TryParse("...");
+
+        // parse.Should.BeTrue();
+        // result.Should().Be(10);
+    }
+
+    [Fact]
+    public void out_Fail()
+    {
+        var str = "string";
+        int result;
+        var target = new CustomInt();
+
+        // TODO 實現一個 int.TryParse
+        // var parse = target.TryParse("...");
+
+        // parse.Should.BeFalse();
+        // result.Should().Be(0);
+    }
+}
+
+public class CustomInt
+{
+}
