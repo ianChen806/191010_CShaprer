8b8f3988ea2d6d8addd134858513792df975356a
IanChen <ian806.chen@gmail.com>
2024-12-30 10:43:54 +0800

    add exercise

---
+++ Exercise/PropertyExercise.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+
+namespace Exercise;
+
+public class PropertyExercise
+{
+    [Fact]
+    public void Method()
+    {
+        Assert.Throws<Exception>(() =>
+        {
+            var service = new BankService();
+            var transfer = new Transfer();
+            transfer.Currency = "TWD";
+            transfer.Amount = -1;
+
+            service.Transfer(transfer);
+        });
+    }
+
+    private class BankService
+    {
+        public void Transfer(Transfer transfer)
+        {
+            // do nothing
+        }
+    }
+
+    private class Transfer
+    {
+        public string Currency { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
