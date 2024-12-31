fd611c8e0ef7d60f04f4b04963a8678fe1b33b39
IanChen <ian806.chen@gmail.com>
2024-12-31 10:20:26 +0800

    adjust final

--- Exercise/FinalExercise.cs
+++ Exercise/FinalExercise.cs
@@ -9,6 +9,7 @@ public class FinalExercise
      * 傳入 Title、Author、Isbn(13碼數字) 建立並初始化物件, Author 未填寫預設為 "Unknown"
      * 提供一個屬性 Isbn, 唯讀不可設定, 返回格式 "ISBN-{Isbn}"
      * 提供一個方法 GetInfo, 返回 Title, Author, Isbn
+     * 不允許外部直接取得 Title、Author，僅可透過GetInfo取得
      * 提供一個索引子 傳入章節號(int)作為索引, 返回或設定章節名稱(string)
      */
     [Fact]
@@ -22,7 +23,7 @@ public class FinalExercise
            {
                 Title = "title",
                 Author = "author",
-                Isbn = "ISBN-9781234567890",
+                Isbn = "9781234567890",
            });
          * book[1] = "Chapter 1";
          * book[1].Should().Be("Chapter 1");
